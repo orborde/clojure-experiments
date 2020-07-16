f9beaac7133fb7af61afac20d8e627114dffe694
William Ehlhardt <williamehlhardt@gmail.com>
2020-07-16 02:00:51 -0700

    solve 177, except for def

--- 4clojure/177.clj
+++ 4clojure/177.clj
@@ -1,18 +1,57 @@
-(def f (fn [s] false))
+(defn f [orig]
+  (def brackets
+    {\{ \},
+     \[ \],
+     \( \)})
+  (defn isstart [c] (not (nil? (brackets c))))
+  (defn isend [c] (not (nil? ((set (vals brackets)) c))))
+  (loop
+      [s orig
+       stack ()]
+    ;(println "running" s stack)
+    (let [c (first s)]
+      (cond
+        (empty? s)
+        (do
+          ;(prn "end")
+          (empty? stack))
+
+        (isstart c)
+        (do
+          ;(println "start" c)
+          (recur
+           (rest s)
+           (conj stack c)))
+
+        (isend c)
+        (do
+          ;(println "end" c)
+          (let [expected (brackets (first stack))]
+            (if (= expected c)
+              (recur (rest s) (rest stack))
+              false))
+          )
+
+        :else
+        (do
+          ;(prn "charprc" c)
+          (recur (rest s) stack)
+          )
+        ))))
 
 (def cases `(
- (f "This string has no brackets.")
- (f "class Test {
+             (f "This string has no brackets.")
+             (f "class Test {
       public static void main(String[] args) {
         System.out.println(\"Hello world.\");
       }
     }")
- (not (f "(start, end]"))
- (not (f "())"))
- (not (f "[ { ] } "))
- (f "([]([(()){()}(()(()))(([[]]({}()))())]((((()()))))))")
- (not (f "([]([(()){()}(()(()))(([[]]({}([)))())]((((()()))))))"))
- (not (f "["))))
+             (not (f "(start, end]"))
+             (not (f "())"))
+             (not (f "[ { ] } "))
+             (f "([]([(()){()}(()(()))(([[]]({}()))())]((((()()))))))")
+             (not (f "([]([(()){()}(()(()))(([[]]({}([)))())]((((()()))))))"))
+             (not (f "["))))
 
 (doall (for [c cases]
-  (println (eval c) c)))
+         (println (eval c) c)))
