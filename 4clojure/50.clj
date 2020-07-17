5a3b48c378d5739e231a6d5d8477ba7ed1dfcb01
William Ehlhardt <williamehlhardt@gmail.com>
2020-07-16 20:05:55 -0700

    solve 50

---
+++ 4clojure/50.clj
@@ -0,0 +1,18 @@
+(def __
+
+(fn [coll]
+  (set (vals (group-by class coll))))
+
+  )
+
+(def cases
+  `(
+    (= (set (__ [1 :a 2 :b 3 :c])) #{[1 2 3] [:a :b :c]})
+    (= (set (__ [:a "foo"  "bar" :b])) #{[:a :b] ["foo" "bar"]})
+    (= (set (__ [[1 2] :a [3 4] 5 6 :b])) #{[[1 2] [3 4]] [:a :b] [5 6]})
+    )
+  )
+
+(doall (for [c cases]
+         (println (eval c) c)))
+
