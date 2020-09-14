d3aeeb77c49febf790b7fc1908b6ebd243f67c5b
William Ehlhardt <williamehlhardt@gmail.com>
2020-09-13 20:22:32 -0700

    fix 144

--- 4clojure/144.clj
+++ 4clojure/144.clj
@@ -1,19 +1,14 @@
 (def __
 
-  (fn [initval & fns]
-    (loop [loopfuns (flatten (repeat fns))
-           result []
-           val initval]
-      (let [newval ((first loopfuns) val)]
-        (lazy-seq
-         (recur (rest loopfuns)
-                (conj result newval)
-                newval)
-         )
-        )
-      )
+  (fn x [initval & fns]
+    (lazy-seq (cons
+               initval
+               (apply x
+                      (concat [((first fns) initval)] (rest fns) [(first fns)])
+                      )
+               )
+              )
     )
-
   )
 
 (prn (= (take 3 (__ 3.14 int double)) [3.14 3 3.0]))
